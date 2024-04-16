adc74cdb8b4e824389077bedc6e19d3d1ad9b15c
kiboma <omayioben@gmail.com>
2024-04-16 07:10:30 +0300

    Create user model and manager

--- user/models.py
+++ user/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    """Manager for users"""
+    def create_user(self, email, password = None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100,unique=True)
+    name = models.CharField(max_length=150)
+    date_of_birth = models.DateField()
+    mobile_number = models.CharField(max_length=100)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
