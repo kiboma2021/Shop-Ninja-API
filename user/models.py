9ac9e50b7856e67b95b279f285f3713f94baa9f2
kiboma <omayioben@gmail.com>
2024-04-16 07:31:46 +0300

    Update user model

--- user/models.py
+++ user/models.py
@@ -15,11 +15,21 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+
+    def create_superuser(self, email, password):
+        """Create a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=100,unique=True)
     name = models.CharField(max_length=150)
-    date_of_birth = models.DateField()
-    mobile_number = models.CharField(max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
+    mobile_number = models.CharField(max_length=100, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
